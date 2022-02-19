d04047ef961fc6acd53edbaed14aae7b39495bf8
roman-kart <romankartasheff@gmail.com>
2022-02-19 23:41:24 +0300

    Простая реализация паттерна Итератор

---
+++ Iterator/CurrentDirectory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    public class CurrentDirectory
+    {
+        public IEnumerable<string> FilesNames(string path)
+        {
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                yield return file;
+            }
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,19 @@
+﻿using Iterator;
+
+Console.WriteLine("Fibonacci: ");
+foreach (var num in GeneratorFibonacci.GenerateFibonacci())
+{
+    if (num < 0)
+    {
+        break;
+    }
+    Console.WriteLine(num);
+}
+
+Console.WriteLine();
+Console.WriteLine($"Files in {Environment.CurrentDirectory}:");
+var dir = new CurrentDirectory();
+foreach (var file in dir.FilesNames(Environment.CurrentDirectory))
+{
+    Console.WriteLine(file);
+}
