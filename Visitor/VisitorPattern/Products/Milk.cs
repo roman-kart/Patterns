240be77110ff0c1fa5f6a71ffdc82154cc2aacfe
roman-kart <romankartasheff@gmail.com>
2022-02-20 20:57:21 +0300

    Добавил простую реализацию паттерна Visitor

---
+++ Visitor/VisitorPattern/Products/Milk.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisitorPattern.Visitor;
+
+namespace VisitorPattern.Products
+{
+    internal class Milk : Product
+    {
+        public override string Name { get; set; }
+        public override string Description { get; set; }
+        public override decimal Price { get; set; }
+
+        public override void Accept(IProductVisitor productVisitor)
+        {
+            productVisitor.Visit(this);
+        }
+    }
+}
---
+++ Visitor/VisitorPattern/Program.cs
@@ -0,0 +1,8 @@
+﻿using VisitorPattern.Visitor;
+using VisitorPattern.Products;
+
+var cereal = new Cereal() { Name = "Tasty cereal", Description = "It's really good!", Price = 100m };
+var milk = new Milk() { Name = "Really good milk", Description = "This milk is really tasty!!!", Price = 70m };
+var productVisitor = new ProductVisitor();
+productVisitor.DoSomething(cereal);
+productVisitor.DoSomething(milk);
