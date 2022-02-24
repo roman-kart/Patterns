2724c39e6cd3f5f667e77282f8e39b1dd8ae3d3f
roman-kart <romankartasheff@gmail.com>
2022-02-24 18:29:41 +0300

    factoryMethod
    Добавил простую реализацию паттерна "Фабричный метод"

---
+++ FactoryMethod/ClassicFactoryMethod/VegetableFactory.cs
@@ -0,0 +1,17 @@
+﻿using FactoryMethod.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.ClassicFactoryMethod
+{
+    public class VegetableFactory : AbstractFactory
+    {
+        public override ProductAbstract GetProduct()
+        {
+            return new ProductVegetables() { Name = "Cucumber", Description = "dsfsdfs", Price = 85 };
+        }
+    }
+}
---
+++ FactoryMethod/WorkWithProducts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryMethod.ClassicFactoryMethod;
+using FactoryMethod.Products;
+
+namespace FactoryMethod
+{
+    public class WorkWithProductClassical
+    {
+        private AbstractFactory _abstractFactory;
+        public WorkWithProductClassical(AbstractFactory abstractFactory)
+        {
+            _abstractFactory = abstractFactory;
+        }
+        public void AboutProduct()
+        {
+            var product = _abstractFactory.GetProduct();
+            Console.WriteLine($"{product.Name} - {product.Price} ({product.Description})");
+        }
+    }
+    public class WorkWithProductStatic
+    {
+        private StaticFactoryMethod.StaticFactoryMethod _staticFactory;
+        public WorkWithProductStatic(StaticFactoryMethod.StaticFactoryMethod factoryMethod)
+        {
+            _staticFactory = factoryMethod;
+        }
+        public void AboutFruit()
+        {
+            var product = _staticFactory.GetProduct(StaticFactoryMethod.ProductKinds.Fruit);
+            Console.WriteLine($"{product.Name} - {product.Price} ({product.Description})");
+        }
+        public void AboutVegetable()
+        {
+            var product = _staticFactory.GetProduct(StaticFactoryMethod.ProductKinds.Vegetables);
+            Console.WriteLine($"{product.Name} - {product.Price} ({product.Description})");
+        }
+    }
+}
